c92c674dc44646c98923fd427ffdde4f44d6584d
LindvedKrvang <rklindved@gmail.com>
2017-08-14 18:24:49 +0200

    Added exercise 1-3 for datastructures

---
+++ August14-2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace August14_2
+{
+    class Program
+    {
+        private static Utility utility;
+
+        static void Main(string[] args)
+        {
+            utility = new Utility();
+            //ExerciseOne();
+            //ExerciseTwo();
+            ExerciseThree();
+        }
+
+        /// <summary>
+        /// Exercise One. Reverse order.
+        /// </summary>
+        private static void ExerciseOne()
+        {
+            Console.WriteLine("Exercise One!");
+            Console.WriteLine("Please enter some text:");
+            var inputOne = utility.PromptText();
+            Console.WriteLine("Please enter some text again:");
+            var inputTwo = utility.PromptText();
+            Console.WriteLine("Please enter some text for the third time:");
+            var inputThree = utility.PromptText();
+
+            Console.WriteLine($"{inputThree}, {inputTwo}, {inputOne}");
+        }
+
+        /// <summary>
+        /// Exercise Two. Print many numbers.
+        /// </summary>
+        private static void ExerciseTwo()
+        {
+            Console.WriteLine("Exercise Two!");
+            Console.WriteLine("Please enter a number:");
+            var number = utility.PromptNumber();
+            Console.WriteLine("Please enter a width:");
+            var width = utility.PromptNumber();
+
+            for (int i = 0; i < width; i++)
+            {
+                var text = "";
+                for (int j = 0; j < width - i; j++)
+                {
+                    text += number;
+                }
+                Console.WriteLine(text);
+            }
+        }
+
+        /// <summary>
+        /// Exercise Three. Login system.
+        /// </summary>
+        private static void ExerciseThree()
+        {
+            Console.WriteLine("Exercise Three!");
+
+            var userId = 10;
+            var password = "admin";
+            var attempts = 3;
+
+            do
+            {
+                Console.WriteLine("Please enter your userID:");
+                var inputId = utility.PromptNumber();
+                Console.WriteLine("Please enter your password:");
+                var inputPassword = utility.PromptText();
+
+                if (userId != inputId || password != inputPassword)
+                {
+                    Console.WriteLine("Sorry. The userID or password does not exits.");
+                    attempts--;
+                    Console.WriteLine($"You have {attempts} tries left..");
+                }
+                else
+                {
+                    Console.WriteLine("You are now logged in.");
+                    return;
+                }
+            } while (attempts > 0);
+            Console.WriteLine("You have used all of your attempts and are now locked out off the system.");
+        }
+    }
+}
---
+++ August14-2/Utility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace August14_2
+{
+    public class Utility
+    {
+        /// <summary>
+        /// Prompts the user for some text.
+        /// </summary>
+        /// <returns></returns>
+        public string PromptText()
+        {
+            var text = Console.ReadLine();
+            while (string.IsNullOrEmpty(text))
+            {
+                Console.WriteLine("I can't use that. Please write something:");
+                text = Console.ReadLine();
+            }
+            return text;
+        }
+
+        /// <summary>
+        /// Prompts for a number. Validates it and returns it.
+        /// </summary>
+        /// <returns></returns>
+        public int PromptNumber()
+        {
+            int number;
+            if (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("That's not a valid number. Try again:");
+                number = PromptNumber();
+            }
+            return number;
+        }
+    }
+}
